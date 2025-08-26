55d94b5ffb97f45a8d075aab9d4fdd437848c389
Jack Benton <jtb.benton@gmail.com>
2025-08-26 16:50:12 -0400

    Completed Flask authenticating users lab

--- server/app.py
+++ server/app.py
@@ -19,38 +19,55 @@ db.init_app(app)
 api = Api(app)
 
 class ClearSession(Resource):
-
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
-
         return {}, 204
 
 class IndexArticle(Resource):
-    
     def get(self):
         articles = [ArticlesSchema().dump(article) for article in Article.query.all()]
         return articles, 200
 
 class ShowArticle(Resource):
-
     def get(self, id):
         session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
         session['page_views'] += 1
 
         if session['page_views'] <= 3:
-
             article = Article.query.filter(Article.id == id).first()
-            article_json = ArticlesSchema.dump(article)
-
+            article_json = ArticlesSchema().dump(article)
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+    def post(self):
+        data = request.get_json() or {}
+        username = data.get('username')
+        user = User.query.filter_by(username=username).first()
+        session['user_id'] = user.id
+        return UserSchema().dump(user), 200
+
+class Logout(Resource):
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if not user_id:
+            return {}, 401
+        user = User.query.get(user_id)
+        return UserSchema().dump(user), 200
+
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
